79921eef46308cc6756ac1d747fcf9cd9d43a696
Ellie <intricatebread@gmail.com>
2021-09-26 15:12:22 -0300

    fixed noop mixin

--- src/main/java/intricarpet/intricarpetExtension.java
+++ src/main/java/intricarpet/intricarpetExtension.java
@@ -1,10 +1,17 @@
 package intricarpet;
 
 import carpet.CarpetExtension;
+import carpet.CarpetServer;
 import intricarpet.logging.intricarpetLoggerRegisty;
 
 public class intricarpetExtension implements CarpetExtension
 {
+    public static void noop() { }
+    static
+    {
+        CarpetServer.manageExtension(new intricarpetExtension());
+    }
+
     @Override
     public void registerLoggers()
     {
