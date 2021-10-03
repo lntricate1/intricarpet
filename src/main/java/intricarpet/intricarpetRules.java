060b414bee3a016c69d5e2802248ff35ab25cb07
Ellie <intricatebread@gmail.com>
2021-10-02 22:21:16 -0300

    Add disableTNTChainReaction

--- src/main/java/intricarpet/intricarpetRules.java
+++ src/main/java/intricarpet/intricarpetRules.java
@@ -8,4 +8,8 @@ public class intricarpetRules
         desc = "EVEN MORE Optimized TNT", category = "intricate",
         extra = {"Stores velocity values, this optimization optimizes many tnt that explode in the same spot. For full effectiveness, also enable optimizedTNT."})
     public static boolean optimizedTNTExtra = false;
+
+    @Rule(
+        desc = "Makes TNT act like normal blocks when blown up", category = "intricate")
+    public static boolean disableTNTChainReaction = false;
 }
