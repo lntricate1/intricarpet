82c82407a9874e8af8e655c6fb58feac416cba4f
Ellie <intricatebread@gmail.com>
2021-10-03 00:33:28 -0300

    Add slimeChunks

--- src/main/java/intricarpet/intricarpetRules.java
+++ src/main/java/intricarpet/intricarpetRules.java
@@ -9,7 +9,12 @@ public class intricarpetRules
         extra = {"Stores velocity values, this optimization optimizes many tnt that explode in the same spot. For full effectiveness, also enable optimizedTNT."})
     public static boolean optimizedTNTExtra = false;
 
-    @Rule(
-        desc = "Makes TNT act like normal blocks when blown up", category = "intricate")
+    @Rule(desc = "Makes TNT act like normal blocks when blown up", category = "intricate")
     public static boolean disableTNTChainReaction = false;
+
+    @Rule(
+        desc = "Makes either all or no chunks behave like Slime Chunks",
+        options = {"vanilla", "all", "none"},
+        category = "intricate")
+    public static String slimeChunks = "vanilla";
 }
