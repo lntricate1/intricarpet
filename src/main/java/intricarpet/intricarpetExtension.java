f3d071153bd6aeb72012ee6d2a8e8260bc61e2d8
Ellie <intricatebread@gmail.com>
2021-10-04 09:56:24 -0300

    Add interaction command

--- src/main/java/intricarpet/intricarpetExtension.java
+++ src/main/java/intricarpet/intricarpetExtension.java
@@ -1,9 +1,14 @@
 package intricarpet;
 
+import com.mojang.brigadier.CommandDispatcher;
+
 import carpet.CarpetExtension;
 import carpet.CarpetServer;
 import carpet.settings.SettingsManager;
+import intricarpet.helpers.Interactions;
 import intricarpet.logging.intricarpetLoggerRegisty;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayerEntity;
 
 public class intricarpetExtension implements CarpetExtension
 {
@@ -37,5 +42,23 @@ public class intricarpetExtension implements CarpetExtension
     public SettingsManager customSettingsManager()
     {
         return intricarpetSettingsManager;
+    }
+
+    @Override
+    public void registerCommands(CommandDispatcher<ServerCommandSource> dispatcher)
+    {
+        InteractionCommand.register(dispatcher);
+    }
+
+    @Override
+    public void onPlayerLoggedIn(ServerPlayerEntity player)
+    {
+        Interactions.onPlayerConnect(player);
+    }
+
+    @Override
+    public void onPlayerLoggedOut(ServerPlayerEntity player)
+    {
+        Interactions.onPlayerDisconnect(player);
     }
 }
