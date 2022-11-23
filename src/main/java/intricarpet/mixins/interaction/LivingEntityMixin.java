b1bcd1c04db1628f8bbace4b0b23d7c737427c4e
wendavid552 <david.jianwei.wen@gmail.com>
2022-11-24 03:22:14 +0800

    Replace some @Overwrite to @Inject to be compatible with other mods.

--- src/main/java/intricarpet/mixins/interaction/LivingEntityMixin.java
+++ src/main/java/intricarpet/mixins/interaction/LivingEntityMixin.java
@@ -1,24 +1,38 @@
 package intricarpet.mixins.interaction;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import intricarpet.helpers.Interactions;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin
 {
-    @Overwrite
-    public boolean isAffectedBySplashPotions()
+    @Inject(method="isAffectedBySplashPotions", at=@At("HEAD"),cancellable = true)
+    public void isAffectedBySplashPotions(CallbackInfoReturnable<Boolean> cir)
     {
         if(((Object) this) instanceof PlayerEntity)
         {
             String playerName = ((PlayerEntity) (Object) this).getName().getString();
             if(Interactions.onlinePlayerMap.containsKey(playerName) &&
-                Interactions.onlinePlayerMap.get(playerName).contains("entities"))
-                return false;
+                Interactions.onlinePlayerMap.get(playerName).contains("entities")) {
+                cir.setReturnValue(false);
+                cir.cancel();
+            }
         }
-        return true;
+    }
+    @Inject(method="isPushable",at=@At("HEAD"),cancellable = true)
+    private void isPushableMixin(CallbackInfoReturnable<Boolean> cir) {
+        if(((Object) this) instanceof PlayerEntity) {
+            String playerName = ((PlayerEntity) (Object) this).getName().getString();
+            if(Interactions.onlinePlayerMap.containsKey(playerName) &&
+                    Interactions.onlinePlayerMap.get(playerName).contains("entities")) {
+                cir.setReturnValue(false);
+                cir.cancel();
+            }
+        }
     }
 }
